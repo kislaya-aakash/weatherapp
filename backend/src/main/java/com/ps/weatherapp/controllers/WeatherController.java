b8977093e2586966ad0e9ee468337dbdf47fd991
KISLAYA AAKASH <kislayaaakash@gmail.com>
2025-01-11 16:41:34 +0530

    change status code

--- backend/src/main/java/com/ps/weatherapp/controllers/WeatherController.java
+++ backend/src/main/java/com/ps/weatherapp/controllers/WeatherController.java
@@ -23,24 +23,24 @@ public class WeatherController {
     @GetMapping("/weather")
     public Mono<ResponseEntity<CityWeatherPrediction>> getWeatherAdvice(@RequestParam String city) {
         return weatherService.getWeatherPrediction(city)
-                .map(cityWeatherPrediction -> {
-                    // Customizing the response body and status code
-                    if (cityWeatherPrediction.getStatus() == 200) {
-                        // Successful response
-                        return ResponseEntity.ok(cityWeatherPrediction); // 200 OK
-                    } else {
-                        // Error response (e.g., city not found or service error)
-                        HttpStatus status = cityWeatherPrediction.getStatus() == 404 ? HttpStatus.NOT_FOUND : HttpStatus.SERVICE_UNAVAILABLE;
-                        return ResponseEntity.status(status) // 404 Not Found
-                                .body(cityWeatherPrediction);
-                    }
-                })
-                .onErrorResume(error -> {
-                    // Handle unexpected errors (e.g., network issues)
-                    CityWeatherPrediction fallbackPrediction = new CityWeatherPrediction(
-                            "Service temporarily unavailable.", new LinkedHashMap<>(), 500);
-                    return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-                            .body(fallbackPrediction)); // 503 Service Unavailable
-                });
+            .map(cityWeatherPrediction -> {
+                // Customizing the response body and status code
+                if (cityWeatherPrediction.getStatus() == 200) {
+                    // Successful response
+                    return ResponseEntity.ok(cityWeatherPrediction); // 200 OK
+                } else {
+                    // Error response (e.g., city not found or service error)
+                    HttpStatus status = cityWeatherPrediction.getStatus() == 404 ? HttpStatus.NOT_FOUND : HttpStatus.SERVICE_UNAVAILABLE;
+                    return ResponseEntity.status(status) // 404 Not Found
+                            .body(cityWeatherPrediction);
+                }
+            })
+            .onErrorResume(error -> {
+                // Handle unexpected errors (e.g., network issues)
+                CityWeatherPrediction fallbackPrediction = new CityWeatherPrediction(
+                        "Service temporarily unavailable.", new LinkedHashMap<>(), 503);
+                return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body(fallbackPrediction)); // 503 Service Unavailable
+            });
     }
 }
